7989cf2b7f56f9ee5a1c3ba0d755572cd1704fb1
Bart Labno <bart.labno@digital.cabinet-office.gov.uk>
2023-11-29 17:14:44 +0000

    disable

--- main.tf
+++ main.tf
@@ -87,17 +87,17 @@ resource "aws_acm_certificate" "cert" {
 #   zone_id         = var.create_route53_hosted_zone ? aws_route53_zone.hosted_zone[0].zone_id : var.route53_hosted_zone_id
 # }
 
-resource "aws_acm_certificate_validation" "cert_validation" {
-  provider = aws.acm_provider
+# resource "aws_acm_certificate_validation" "cert_validation" {
+#   provider = aws.acm_provider
 
-  # Dependency to guarantee that certificate and DNS records are created before this resource
-  depends_on = [
-    aws_acm_certificate.cert,
-    aws_route53_record.acm_certificate_validation_records,
-  ]
+#   # Dependency to guarantee that certificate and DNS records are created before this resource
+#   depends_on = [
+#     aws_acm_certificate.cert,
+#     aws_route53_record.acm_certificate_validation_records,
+#   ]
 
-  count = var.create_acm_certificate ? 1 : 0
+#   count = var.create_acm_certificate ? 1 : 0
 
-  certificate_arn         = aws_acm_certificate.cert[0].arn
-  validation_record_fqdns = [for record in aws_route53_record.acm_certificate_validation_records : record.fqdn]
-}
+#   certificate_arn         = aws_acm_certificate.cert[0].arn
+#   validation_record_fqdns = [for record in aws_route53_record.acm_certificate_validation_records : record.fqdn]
+# }
